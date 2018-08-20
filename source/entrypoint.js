57e9b005e5c0917d34fb9963398efcd45fc61260
Dr. Safi <sfmissive@gmail.com>
2018-08-21 01:27:01 +0300

    Initial upload.

---
+++ source/entrypoint.js
@@ -0,0 +1,23 @@
+export function streamToString(stream, enc, cb) {
+  if (typeof enc === 'function') {
+      cb = enc
+      enc = null
+  }
+  cb = cb || function () {}
+
+  var str = ''
+
+  return new Promise (function (resolve, reject) {
+      stream.on('data', function (data) {
+          str += (typeof enc === 'string') ? data.toString(enc) : data.toString()
+      })
+      stream.on('end', function () {
+          resolve(str)
+          cb(null, str)
+      })
+      stream.on('error', function (err) {
+          reject(err)
+          cb(err)
+      })
+  })
+}
